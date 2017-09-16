709cafde4ba2b88aa395a4dfe47c089036d2861d
Unknown <wesleyaegaz@gmail.com>
2017-09-16 11:34:42 -0400

    Added initializer for Event class

--- timemgmt.py
+++ timemgmt.py
@@ -1,7 +1,29 @@
 import icalendar
 import os
+import datetime
 
-if os.path.exists("event.ics"):
+class event:
+    #dateStart
+    #dateEnd
+    #type
+    #Flexibility Boolean
+    def __init__(self, dateS, dateE, summary, flexibilityFactor=False):
+        self.isFlexible = flexibilityFactor
+        self.dateStart = dateS
+        self.dateEnd = dateE
+        timeTypes = ["sleep", "meal", "exercise", "leisure", "study", "class", "obligation", "gap", "transition", "earlyWakeTime", "lateWake", "earlySleep", "lateSleep"]
+        matched = False
+        for category in timeTypes:
+            if '%'+category+'%' in summary:
+                self.timeType = category
+                matched = True
+                break
+        if not matched:
+            #Prompt user for category type
+            self.timeType = "obligation"
+        
+
+if os.path.exists("wfholman@umail.iu.edu.ics"):
     print("Load the string")
     with open('wfholman@umail.iu.edu.ics', 'r') as myfile:
         data=myfile.read()
