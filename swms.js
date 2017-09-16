5208fa86688c345026b00387320131154e73507a
Seth T <seth10@users.noreply.github.com>
2017-09-16 04:15:23 -0400

    Obfuscate Discord token with a micro Node module
    Also print a log message with the user who sent a recieved message, and ignore vim swap files.

--- swms.js
+++ swms.js
@@ -1,14 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const auth_token = require('swms.token');
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
+    console.log("message from " + message.author.username);
     if (message.content === 'ping') {
         message.reply('pong');
     }
 });
 
-client.login('token');
+auth_token(client);
