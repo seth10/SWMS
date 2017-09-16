f5ddb3febd46a7d784614973ee65c8b3a1c74ad6
Seth T <seth10@users.noreply.github.com>
2017-09-16 05:38:49 -0400

    Send direct messages

--- swms.js
+++ swms.js
@@ -3,26 +3,30 @@ const client = new Discord.Client();
 const token_login = require('swms-token');
 const schedule = require('node-schedule');
 
+const morning_messages = [
+    'Good morning! On a scale of 1-10, how much energy do you have?'
+];
+
+function getGratitudeMessage(userID) {
+    const gratitude_messages = [
+        `Thank you for your response, <@${userID}> :smiley:`
+    ]
+    return gratitude_messages[Math.floor(Math.random()*gratitude_messages.length)];
+}
+
 client.on('ready', () => {
     let message = 'SWMS Discord chatbot ready!';
     console.log(message);
-    client.channels.find('name', 'bot').send(message);
 });
 
 client.on('message', message => {
     if (message.author.username != client.user.username)
-        console.log('message from ' + message.author.username);
-    if (message.content === 'ping')
-        message.reply('pong');
+        message.author.send(getGratitudeMessage(message.author.id));
 });
 
-const morning_messages = [
-    'Good morning! On a scale of 1-10, how much energy do you have?',
-];
-//const morning_notification = schedule.scheduleJob('0 9 * * 1-5', () => {
-const morning_notification = schedule.scheduleJob('1 * * * *', () => {
+const morning_notification = schedule.scheduleJob('0 9 * * 1-5', () => {
     let message = morning_messages[Math.floor(Math.random()*morning_messages.length)];
-    client.channels.find('name', 'bot').send(message);
+    client.users.find('username', 'Seth').send(message);
 });
 
 token_login(client);
